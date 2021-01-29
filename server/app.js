44d2e2f01e545c13257c9198d63d3f6e0765a505
phucciTr <miabam26@gmail.com>
2021-01-28 19:59:26 -0800

    Parsed JSON on server side:

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
 
-app.use('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, './..public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('hello csv');
 });
 
 app.post('/', (req, res) => {
+  console.log('req.body = ', req.body);
   res.send('hello post csv');
 });
 
