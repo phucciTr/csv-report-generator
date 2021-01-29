b672d26efe8b7cf624784afc50732450d9f01db2
phucciTr <miabam26@gmail.com>
2021-01-29 15:37:32 -0800

    Converted JSON to CSV

--- server/app.js
+++ server/app.js
@@ -5,21 +5,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { csvParser } = require('./middleware/csvParser');
 
+
 app.use('/', express.static(path.join(__dirname, './..public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(csvParser);
+
 
 app.get('/', (req, res) => {
   res.send('hello csv');
 });
 
 app.post('/', (req, res) => {
-
-  let parsedJSON = JSON.parse(req.body.data);
-  // console.log('parsed req.body.data = ', parsedJSON);
-  // console.log('parsed req.body.data.children[0].children = ', parsedJSON.children[0].children);
-  // console.log('typeof parsed req.body.data = ', typeof parsedJSON);
-
   res.send('hello post csv');
 });
 
--- server/middleware/csvParser.js
+++ server/middleware/csvParser.js
@@ -1,14 +1,43 @@
-let test = 'testing promise';
 
 const csvParser = (req, res, next) => {
-  Promise.resolve(test)
-    .then((result) => {
-      console.log('result = ', result);
-    })
-    .catch((err) => {
-      console.log('err = ', err);
-    });
+  let parsedJSON = JSON.parse(req.body.data);
+
+
+  let csv = createColsName(parsedJSON);
+  csv = populateData(parsedJSON, csv);
+  console.log(csv);
 };
+
+
+var createColsName = (data) => {
+  let columns = Object.keys(data);
+
+  return columns.filter((colName) => colName !== 'children').reduce((csv, col, index) => {
+    return csv += (index !== (columns.length - 2)) ? `${col},` : `${col}\n`;
+  }, '');
+};
+
+var populateData = (data, csv) => {
+
+  for (let key in data) {
+    let value = data[key];
+
+    if (isPrimitive(value)) {
+      csv += (key === 'sales') ? `${value}\n` : `${value},`;
+    }
+
+    if (isIterable(value)) {
+      for (let obj of value) {
+        csv = populateData(obj, csv);
+      }
+    }
+  }
+
+  return csv;
+};
+
+var isIterable = (value) => Array.isArray(value) && value.length > 0;
+var isPrimitive = (value) => typeof value !== 'object';
 
 module.exports = {
   csvParser : csvParser
